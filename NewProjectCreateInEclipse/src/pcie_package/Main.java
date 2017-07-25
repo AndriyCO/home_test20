80a26e0516bbe024f5cf6d4a67e61edbf0ad1e3f
AndriyCO <Aolijnyk73@gmail.com>
2017-07-25 17:52:36 +0300

    it not first commit

--- NewProjectCreateInEclipse/src/pcie_package/Main.java
+++ NewProjectCreateInEclipse/src/pcie_package/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("The product is still in development");
-
+		System.out.println("message in collateral branch");
 	}
 
 }
