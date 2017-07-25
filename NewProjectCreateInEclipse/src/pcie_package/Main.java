087652b02fdca5392e59c2b7970e305000ac82a7
AndriyCO <Aolijnyk73@gmail.com>
2017-07-25 17:48:58 +0300

    first on paralel branch

--- NewProjectCreateInEclipse/src/pcie_package/Main.java
+++ NewProjectCreateInEclipse/src/pcie_package/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("The product is still in development");
+		System.out.println("add new message to master-branch method");
 
 	}
 
